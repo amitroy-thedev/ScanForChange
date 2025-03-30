fcd188ef2587fd980c37d4141414d23225597fa0
Amit Roy <amitroy.thedev@gmail.com>
2025-03-30 14:50:25 +0530

    dashboard update

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -40,16 +40,10 @@ export default function UserDetails() {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-2xl font-bold">User Details</h1>
-      <p>Email: {userData.email}</p>
-      <p>Joined At: {new Date(userData.joinedAt).toLocaleDateString()}</p>
-      <p>Points: {userData.points}</p>
-      <p>Rank: {userData.rank}</p>
-
-      <h2 className="text-xl font-semibold mt-4">Reports</h2>
+      <h2 className="text-xl font-semibold mt-4">Recent Activity</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {userData.reports.map((report, index) => (
-          <div key={index} className="border p-4 rounded-md shadow-md">
+          <div key={index} className=" p-4 rounded-md shadow-md">
             <img
               src={report.imgUrl}
               alt="Reported Waste"
